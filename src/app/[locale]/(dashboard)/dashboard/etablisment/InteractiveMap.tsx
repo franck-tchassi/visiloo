39be30bc48b2fddf125b1ab76cc53398be3d6505
franck-tchassi <franck.tchassi@laplateforme.io>
2025-09-22 12:26:39 +0200

    modification du package.json

--- src/app/[locale]/(dashboard)/dashboard/etablisment/InteractiveMap.tsx
+++ src/app/[locale]/(dashboard)/dashboard/etablisment/InteractiveMap.tsx
@@ -4,8 +4,8 @@ import React, { useEffect } from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
-import 'leaflet.markercluster/dist/MarkerCluster.css';
-import 'leaflet.markercluster/dist/MarkerCluster.Default.css';
+//import 'leaflet.markercluster/dist/MarkerCluster.css';
+//import 'leaflet.markercluster/dist/MarkerCluster.Default.css';
 import 'leaflet.markercluster';
 import { MapPin } from 'lucide-react';
 
