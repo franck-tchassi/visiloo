a4aa4dec7eb9e0aad5c51a3f544cb97a27c63af1
franck-tchassi <franck.tchassi@laplateforme.io>
2025-09-22 12:04:50 +0200

    mise a jour du layout

--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -4,22 +4,28 @@ declare module "next-auth" {
   interface Session {
     user: {
       id: string;
-      role: "USER" | "ADMIN";
-      currentOrganizationId?: string; // Ajoute l'ID de l'organisation actuelle
-      locale?: string; // Ajoute la locale de l'utilisateur
+      role: "USER" | "MEMBER" | "ADMIN"; // Ajoutez MEMBER
+      stripeCustomerId?: string | null;
+      currentOrganizationId?: string | null;
+      selectedEstablishmentId?: string | null;
+      locale?: string;
     } & DefaultSession["user"];
   }
 
   interface User {
     id: string;
-    role: "USER" | "ADMIN";
-    currentOrganizationId?: string; // Ajoute l'ID de l'organisation actuelle
+    role: "USER" | "MEMBER" | "ADMIN"; // Ajoutez MEMBER
+    stripeCustomerId?: string | null;
+    currentOrganizationId?: string | null;
+    selectedEstablishmentId?: string | null;
   }
 }
 
 declare module "next-auth/jwt" {
   interface JWT {
-    role: "USER" | "ADMIN";
-    currentOrganizationId?: string; // Ajoute l'ID de l'organisation actuelle
+    role: "USER" | "MEMBER" | "ADMIN"; // Ajoutez MEMBER
+    stripeCustomerId?: string | null;
+    currentOrganizationId?: string | null;
+    selectedEstablishmentId?: string | null;
   }
 }
